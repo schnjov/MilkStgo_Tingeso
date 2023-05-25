ba08a79705b7a6adfa89fc32daf4daf8402358f4
Jovanni Schneider <jovanni.schneider@usach.cl>
2023-05-25 14:38:55 -0400

    [Fix] Coverage

--- src/test/java/cl/usach/tingeso/sistemamilkstgo/Services/ValoresAcopioServiceTest.java
+++
@@ -1,9 +0,0 @@
-package cl.usach.tingeso.sistemamilkstgo.Services;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class ValoresAcopioServiceTest {
-
-}
